262861edffec44e58c4012e57bc3be1f98160343
LadyAnith <LadyAnith@gmail.com>
2020-09-08 14:27:14 +0200

    Crud Usuarios

--- src/Controller/UsuariosController.php
+++ src/Controller/UsuariosController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Usuario;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class UsuariosController extends AbstractController
 {
@@ -12,8 +15,57 @@ class UsuariosController extends AbstractController
      */
     public function index()
     {
+        //vuelco el listado en el render:
+        $entityManager = $this->getDoctrine()->getManager();
+        $usuarioRepo = $this->getDoctrine()->getRepository(Usuario::class);
+
+        //Guardo en una variable todo el contenido de la variable anterior, que lo interpreta como un array:
+        $usuarios = $usuarioRepo->findAll();
+
         return $this->render('usuarios/index.html.twig', [
             'controller_name' => 'UsuariosController',
+            //Aquí indico el nombre de la variable donde guardo todo el listado, y le doy un name
+            'usuarios'=>$usuarios,
         ]);
     }
+
+    /**
+     * @Route("/altaUsuario", name="altaUsuario", methods={"POST"})
+     */
+
+    public function altaUsuario(Request $request): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $usuario = new Usuario();
+
+        $usuario->setNombre($request->request->get('nombre'))
+            ->setContrasenia(md5($request->request->get('contrasenia')));
+
+        $entityManager->persist($usuario);
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('usuarios');
+    }
+
+    /**
+     * @Route("/bajaUsuario/{id}", name="bajaUsuario")
+     */
+    public function bajaUsuario($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $usuarioRepo = $this->getDoctrine()->getRepository(Usuario::class);
+
+        $usuario = $usuarioRepo->find($id);
+
+        $entityManager->remove($usuario);
+        $entityManager->flush();
+
+        $usuarios = $usuarioRepo->findAll();
+
+        return $this->redirectToRoute('usuarios');
+
+    }
+
+
 }
