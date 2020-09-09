4bf874565bbec473cc2cf07003609ea9adbf90c1
LadyAnith <LadyAnith@gmail.com>
2020-09-09 19:04:42 +0200

    listado

--- src/Controller/PlatosController.php
+++ src/Controller/PlatosController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Plato;
+use App\Entity\TipoPlato;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -15,12 +16,20 @@ class PlatosController extends AbstractController
      */
     public function index()
     {
+        $entityManager = $this->getDoctrine()->getManager();
         //Aqui irá el select de platos
+        $platosRepo = $this->getDoctrine()->getRepository(Plato::class);
+        $platos = $platosRepo->findAll();
         //aqui irá el select de tipos de platos
+        $tipoPlatosRepo = $this->getDoctrine()->getRepository(TipoPlato::class);
+        $tipoPlatos = $tipoPlatosRepo->findAll();
+
 
         return $this->render('platos/index.html.twig', [
             'controller_name' => 'PlatosController',
             //Añadir aqui ambas variables de los listados
+            'platos'=>$platos,
+            'tipoPlatos'=>$tipoPlatos,
         ]);
     }
 
@@ -36,9 +45,13 @@ class PlatosController extends AbstractController
         $caloriasString = $request->request->get('calorias');
         $calorias = floatval(str_replace(',', '.', str_replace('.', '', $caloriasString)));
 
+        //Convertimos el value de tipoPlatos en int
+        $tipoString = $request->request->get('tipoPlatos');
+        $tipo = intval($tipoString );
+
         $plato->setNombre($request->request->get('nombre'))
             ->setCalorias($calorias)
-            ->setCalorias($request->request->get('tipoPlatos'));
+            ->setTipo($tipo);
 
         $entityManager->persist($plato);
         $entityManager->flush();
