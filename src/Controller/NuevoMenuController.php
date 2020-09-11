ea10072b2b55060637e7a466dffd31bae495e601
LadyAnith <LadyAnith@gmail.com>
2020-09-11 11:55:18 +0200

    gestor menu

--- src/Controller/NuevoMenuController.php
+++ src/Controller/NuevoMenuController.php
@@ -16,4 +16,36 @@ class NuevoMenuController extends AbstractController
             'controller_name' => 'NuevoMenuController',
         ]);
     }
+
+    
+    /**
+     * @Route("/generarMenu", name="generar_menu")
+     */
+    public function generarMenu()
+    {
+        //Aqui iria lo del generador de PDF que tengo en trello
+        
+        return $this->render('nuevo_menu/index.html.twig', [
+            'controller_name' => 'NuevoMenuController',
+        ]);
+    }
+
+    /* guia pARA PILLAR PLATOS
+        //Pillo los tipos de comidas
+        $tipoDesatyuno = repository-finb(1)
+        $tipoCena,,, find(2)
+
+        //cONSTRUIR FILTRO
+        $filtro = array('tipo' => $tipoDesayuno);
+        //bUSCAR PLATOS DEL FILTRO
+        $PLAYOSdESAYUNO = $em->getRepository('SomeEntity')->findBy($filtro);
+
+
+        //cONSTRUIR FILTRO
+        $filtro = array('tipo' => $tipocENA);
+        //bUSCAR PLATOS DEL FILTRO
+        $PLAYOSdESAYUNO = $em->getRepository('SomeEntity')->findBy($filtro);
+
+
+    */
 }
